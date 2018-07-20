3c36fab6f7739bdc1aed2670c16eae20a823a886
Cherrary <xc20160917@163.com>
2018-07-20 15:03:24 +0800

    add client processing data

--- Redis_test/server.py
+++ Redis_test/server.py
@@ -59,3 +59,67 @@ class Client(object):
 					raise e
 				else:
 					continue
+			data = yield self.connection.read_line()
+			if not data:
+				if not n_tries:
+					raise ConnectionError("no data received")
+			else:
+				resp = self.process_data(data, cmd_line)
+				if isinstance(resp, partial):
+					resp = yield resp()
+				result = self.format_reply(cmd_line, resp)
+				break
+		return result
+
+	#processing bulk data
+	@gen.coroutine
+	def _consume_bulk(self, tail):
+		response = yield self.connection.read(int(tail) + 2)
+		if isinstance(response, Exception):
+			raise response
+		if not response:
+			raise ResponseError("Empty responsce")
+		else:
+			response = to_unicode(response)
+			response = response[:-2]
+		return response
+	
+	#according to redis protocol to judgment whether there is subsequence data to processing
+	def process_data(self, data, cmd_line):
+		data = to_basedtring(data)
+		data = data[:-2]
+		if data == '$-1':
+			response = None
+		elif data == '*0' or data == '*-1':
+			response = []
+		else:
+			head, tail = data[0], data[1:]
+			if head = '*':
+				return partical(self.consume_multibulk, int(tail), cmd_line)
+			elif head == '$':
+				return partical(self._consume_bluk, tail)
+			elif head == '+':	
+				response = tail
+			elif head == ':':
+				response = int(tail)
+			elif head == '-':
+				if tail.startswith('ERR'):
+					tail = tail[4:]
+				response = ResponseError(tail, cmd_line)
+			else:
+				raise ResponseError("Unkown response type %s" % head, cmd_line)
+		return response
+
+	#set key
+	@gen.coroutine
+	def set(self, key, value):
+		result = yield self.execute_command("SET", key, value)
+		return result
+
+	#get key
+	@gen.coroutine	
+	def get(self, key):
+		value = yield self.execute_command("GET", key)	
+		return value
+
+
